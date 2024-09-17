868cee7aebef177b47a7d2e5912d1d761ac8d46f
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-09-17 14:46:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -31,11 +31,11 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "vipurl.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "af62856348ba8db834ac180c8543df3a1814d1c8")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "instantearn.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "ddd4bfc28cb20195c91c2aa6c7136565a0b47414")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_open_link_rockersbot/19")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_open_link_rockersbot/22")
 
 
 #force sub channel id, if you want enable force sub
