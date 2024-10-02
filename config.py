3c315230ee06e8bde5c28a5a8481f11330940fa0
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-10-02 19:10:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "short2url.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "67921e2a3b356b30e41502b14401f0f608bf6d19")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "028dcc84c348788150cdbc48331a813bf7a8d257")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_open_link_rockersbot/22")
